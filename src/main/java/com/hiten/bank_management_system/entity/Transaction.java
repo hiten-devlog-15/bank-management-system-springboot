bda572aec2f3537f64be006c47838080d7497e1f
Hiten Nath <2024.hiten.nath@ves.ac.in>
2026-08-16 00:05:44 +0530

    Created Transaction entity

--- src/main/java/com/hiten/bank_management_system/entity/Transaction.java
+++ src/main/java/com/hiten/bank_management_system/entity/Transaction.java
@@ -1,8 +1,34 @@
 package com.hiten.bank_management_system.entity;
 
-public class Transaction {
+import com.hiten.bank_management_system.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
     private long transactionId;
 
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_type")
+    private TransactionType transactionType;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Column(name = "date_of_transaction")
+    private LocalDate date;
+
+    @Column(name = "balance_after_transaction")
+    private double balanceAfterTransaction;
 }
